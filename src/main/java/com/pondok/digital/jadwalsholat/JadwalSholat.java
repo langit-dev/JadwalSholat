0df76dfabde9fab49d6bcbadb15ce7fb7d66579a
pondokdigitalindonesia <office@pondokdigital.biz.id>
2024-09-18 01:13:50 +0700

    first commit

---
+++ src/main/java/com/pondok/digital/jadwalsholat/JadwalSholat.java
@@ -0,0 +1,8 @@
+package com.pondok.digital.jadwalsholat;
+
+public class JadwalSholat {
+    public String getPrayerTimes() {
+        // Implementasikan logika untuk mendapatkan jadwal sholat di sini
+        return "Jadwal Sholat";
+    }
+}
