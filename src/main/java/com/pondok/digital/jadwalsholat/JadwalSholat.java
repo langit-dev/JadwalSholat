6ae8be9fd131c021bc31e8da85e7228810070aee
pondokdigitalindonesia <office@pondokdigital.biz.id>
2024-09-18 04:26:09 +0700

    new

--- src/main/java/com/pondok/digital/jadwalsholat/JadwalSholat.java
+++
@@ -1,8 +0,0 @@
-package com.pondok.digital.jadwalsholat;
-
-public class JadwalSholat {
-    public String getPrayerTimes() {
-        // Implementasikan logika untuk mendapatkan jadwal sholat di sini
-        return "Jadwal Sholat";
-    }
-}
